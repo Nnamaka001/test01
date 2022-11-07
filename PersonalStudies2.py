7e3cdbc6cd70b31fce01dbeeef0ad23c94650326
Nnamaka001 <nnamaka.azubuike@yahoo.com>
2022-11-07 23:06:17 +0100

    first commit

---
+++ PersonalStudies2.py
@@ -0,0 +1,55 @@
+'''class myclass():
+    def __len__(self):
+        return 0
+myobj = myclass()
+print(bool(myobj))'''
+
+'''def myFunction():
+    return True
+print(myFunction())'''
+
+'''def myFunction():
+    return False
+if myFunction():
+    print('Yes!')
+else:
+    print('No!')'''
+
+'''x = 200
+print(isinstance(x, float))'''
+
+'''mylist = list(('Pineapple', 'Water melon', 'Berry'))
+print(mylist)'''
+
+'''fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+newlist = []
+for x in fruits:
+    if 'a' in x:
+        newlist.append(x)
+print(newlist)'''
+
+'''fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+newlist = [x for x in fruits if 'a' in x]
+print(newlist)'''
+
+'''newlist = [x for x in range(10) if x < 5]
+print(newlist)'''
+
+'''fruits = ['xapple', 'manana', 'yherry', 'kiwi', 'mango']
+fruits.sort(reverse = True)
+print(fruits)'''
+
+'''fruits = ['apple', 'Banana', 'Zerry', 'Kiwi', 'mango']
+myList = list(fruits)
+print(myList)'''
+
+'''mytuple = ('apple', 'banana', 'cherry', 'strawberry', 'raspberry')
+i = 0
+while i < len(mytuple):
+    print(mytuple[i])
+    i = i + 1'''
+
+'''fruits = {'apple', 'banana', 'cherry'}
+tropical = {'pineapple', 'mango', 'papaya', 'apple'}
+z = fruits.symmetric_difference(tropical)
+print(z)'''
